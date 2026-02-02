f6feb7ecc7dc977790aaa3661b9522865e635829
cavagugo <cavagugo@gmail.com>
2026-02-02 16:02:49 -0600

    Controles y fisica basicos
    El jugador puede ir de lado a lado, saltando constantemente. Las plataformas ignoran colisiones que vienen desde abajo y el jugador solo colisiona con la plataforma si viene desde arriba.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Checa si el jugador viene de arriba
+        if (collision.relativeVelocity.y <= 0)
+        {
+            //Hace que el jugador salte con una fuerza constante sin importar desde qué tan alto o bajo caiga
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                Vector2 velocity = rb.velocity;
+                velocity.y = jumpForce;
+                rb.velocity = velocity;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    float movement = 0f;
+    public float speed = 10f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * speed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
